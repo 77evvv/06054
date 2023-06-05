e75f42dc16b098553be4ab91f0f62167845dfaa9
evachang <mins89157@gmail.com>
2023-06-05 13:15:16 +0800

    123

--- Assets/S1mgr.cs
+++ Assets/S1mgr.cs
@@ -7,7 +7,7 @@ public class S1mgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("OK");
     }
 
     // Update is called once per frame
